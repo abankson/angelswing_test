becc2dea7b0f4d74ea2a0fe34ff1d23a6fb10023
abankson <adam.bankson@gmail.com>
2018-05-23 17:45:23 +0900

    Added the show and new controller actions and views for Projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,25 @@ class ProjectsController < ApplicationController
   def index 
     @project_items = Project.all
   end
+
+  def new
+    @project_item = Project.new
+  end
+
+  def create
+    @project_item = Project.new(params.require(:project).permit(:title, :description, :thumbnail))
+
+    respond_to do |format|
+      if @project_item.save
+        format.html { redirect_to @project_item, notice: 'Your project has been created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def show
+    @project_item = Project.find(params[:id])
+  end
+
 end
