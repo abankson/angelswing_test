cec3e5c8afc6021609afbfaccf78f3e7a7d242ba
abankson <adam.bankson@gmail.com>
2018-05-24 00:37:41 +0900

    Cleaned up code in projects controller and added validation of field title

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index 
     @project_items = Project.all
@@ -9,7 +10,7 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    @project_item = Project.new(params.require(:project).permit(:title, :description, :thumbnail))
+    @project_item = Project.new(project_params)
 
     respond_to do |format|
       if @project_item.save
@@ -21,18 +22,14 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    @project_item = Project.find(params[:id])
   end
 
   def edit
-    @project_item = Project.find(params[:id])
   end
 
   def update
-    @project_item = Project.find(params[:id])
-
     respond_to do |format|
-      if @project_item.update(params.require(:project).permit(:title, :description, :thumbnail))
+      if @project_item.update(project_params)
         format.html { redirect_to projects_path, notice: 'Project was successfully updated.' }
       else
         format.html { render :edit }
@@ -41,8 +38,6 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project_item = Project.find(params[:id])
-
     #destroying the object
     @project_item.destroy
 
@@ -52,4 +47,14 @@ class ProjectsController < ApplicationController
     end
   end
 
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_project
+      @project_item = Project.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def project_params
+      params.require(:project).permit(:title, :description, :thumbnail)
+    end
 end
