d6c72206c69f6f261f4503d20df868947c571d23
abankson <adam.bankson@gmail.com>
2018-05-24 09:32:17 +0900

    Added index functionality for project contents

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,2 +1,25 @@
 class ContentsController < ApplicationController
+
+  def index
+    @content_items = Content.all
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
