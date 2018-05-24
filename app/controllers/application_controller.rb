1fa940c28f689e098ea5989eb1dd3d2320f2d437
abankson <adam.bankson@gmail.com>
2018-05-24 13:55:53 +0900

    Added guest user for current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   #DeviseWhitelist(In Concerns) has controller actions for allowing new parameters to the User devise model
   include DeviseWhitelist
+  #CurrentUserConcern(In Concerns) sets guest user is not current_user
+  include CurrentUserConcern
 
 end
