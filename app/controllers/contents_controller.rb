de030686317172ea1026a82751a13383885b1f87
abankson <adam.bankson@gmail.com>
2018-05-24 09:56:36 +0900

    Added some functionality in the contents controller as well as implemented the show page

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,4 +1,5 @@
 class ContentsController < ApplicationController
+  before_action :set_content, only: [:show, :edit, :update, :destroy]
 
   def index
     @content_items = Content.all
@@ -22,4 +23,14 @@ class ContentsController < ApplicationController
   def destroy
   end
 
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_content
+      @content_item = Content.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def content_params
+      params.require(:content).permit(:title, :content_type, :project_id)
+    end
 end
